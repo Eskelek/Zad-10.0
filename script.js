cf0a8d3e44601aa9af1fce8d9df5b83b8d953c91
eskel <lorthan966@wp.pl>
2017-06-15 15:42:54 +0200

    fin

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(function () {
-    var carouselList = $("#carousel ul");
+    var carouselList = $("#carousel ul"),
+        intervalId;
     
-    setInterval(swapImages, 3000);
+    intervalId = setInterval(changeRightSlide, 3000);
     
-    function swapImages () {
-        carouselList.animate({'marginLeft':-400}, 700, moveFirstSlide);
+    function changeRightSlide() {
+        carouselList.animate({marginLeft: -400},700, moveFirstSlide);
+    }
+    function changeLeftSlide() {
+        moveLastSlide();
+        carouselList.animate({
+            marginLeft: 0
+        }, 700);
     }
     
     function moveFirstSlide () {
@@ -14,20 +21,33 @@ $(function () {
         carouselList.css({marginLeft: 0});
     }
     
+    function moveLastSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft: -400});
+    }
+    
     var swap = $("#carousel div");
     
     $(swap).css({opacity: 0});
-    $(swap).mouseover(function(){
+    $(swap).mouseover(function() {
+        clearInterval(intervalId);
         swap.css({opacity: 0.6});
     })
-    $(swap).mouseout(function(){
+    $(swap).mouseout(function() {
+        intervalId = setInterval(changeRightSlide, 3000);
         swap.css({opacity: 0});
     });
     
     var rArrowClick = $("#right");
     var lArrowClick = $("#left");
     
-    $(rArrowClick).click(function(){
-        console.log("right");
+    $(rArrowClick).click(function() {
+        changeRightSlide();
+    });
+    $(lArrowClick).click(function() {
+        changeLeftSlide();
     })
 });
